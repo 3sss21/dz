f406b0b6f84ae114e624e8284275a96391215afc
3sss21 <asultanoov@gmail.com>
2022-03-15 13:23:42 +0300

    dz

---
+++ bin/lessons11.dart
@@ -0,0 +1,37 @@
+import 'dart:svg';
+
+void main(List<String> arguments) {
+  // print('Hello world!');
+  // var a= 12;
+  // String name = "aidar";
+  // String b = " buster";
+  // var c =name +name;
+  
+  // int num2= 23;
+  // print(num2, name);
+  // int a=1; 
+  // int b=23;
+  // print(a+b);
+  // String name = 'My name is  "Aidar"';
+  // print(name);
+
+  // bool isTrue = true;
+  // print(isTrue);
+  // List  map = [1, 2, "aidar", true];
+  // print(map);
+  // print(map.runtimeType);
+  // Map map1 = {
+  //   'name':'aidar',
+  //   'age':23,
+  //  };
+  //  print(map1);
+  // dynamic number = "aidar";
+  // print(number);
+  /*final i;
+  i = 12;
+  
+  const name=12;
+
+  var mag;
+  bool isNull =true;*/
+}
